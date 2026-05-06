bab6d11d4aeece19340cb93f4087b9361e570f2a
Zanou24 <alexandre.zanelly1@gmail.com>
2026-05-06 14:57:01 +0200

    Create About page

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,3 +1,37 @@
+import about from "../assets/img/about.jpg";
+import "../assets/styles/About.sass";
+import Collapses from "../components/collapses";
+
+const items = [
+  {
+    id: 1,
+    title: "Fiabilité",
+    content:
+      "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+  },
+  { id: 2, title: "Respect", content: "..." },
+  { id: 3, title: "Service", content: "..." },
+  { id: 4, title: "Sécurité", content: "..." },
+];
+
 export default function About() {
-  return <h2>About</h2>;
+  return (
+    <section className="about">
+      <img
+        src={about}
+        alt="Image de présentation de la page à propos"
+        className="about_intro"
+      />
+      <div className="collapses">
+        {items.map((item) => (
+          <Collapses
+            key={item.id}
+            title={item.title}
+            content={item.content}
+            className="about_collapses"
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
--- Frontend/src/pages/housing.jsx
+++ Frontend/src/pages/housing.jsx
@@ -1,17 +1,21 @@
 import { useParams } from "react-router";
 import Data from "../api.json";
+import NotFound from "./NotFound";
 
 export default function Housing() {
   const { id } = useParams();
   const housing = Data.find((item) => item.id === id);
 
-  if (!housing) return <NotFound />;
+  // if (housing === undefined) return <NotFound type="housing" />;
+
+  let index = 0;
 
   return (
     <div>
       <h1>{housing.title}</h1>
-      <img src={housing.cover} alt={housing.title} />
-      {/* autres infos */}
+      <div>
+        <img src={housing.pictures[index]} alt={housing.title} />
+      </div>
     </div>
   );
 }
